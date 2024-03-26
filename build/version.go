09e1485491f4577df006c9312991ee38065b337d
riguang <zscboy@gmail.com>
2024-03-26 18:18:22 +0800

    v0.1.15

--- build/version.go
+++ build/version.go
@@ -24,7 +24,7 @@ func BuildTypeString() string {
 }
 
 // BuildVersion is the local build version
-const BuildVersion = "0.1.14"
+const BuildVersion = "0.1.15"
 
 func UserVersion() string {
 	if os.Getenv("TITAN_VERSION_IGNORE_COMMIT") == "1" {
