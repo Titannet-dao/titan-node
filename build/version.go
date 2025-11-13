a62ef10cceedea82fd34ea174ac3ede4ff166fbd
riguang <zscboy@gmail.com>
2025-11-13 16:22:18 +0800

    Add default http3 client to fix port abuse

--- build/version.go
+++ build/version.go
@@ -24,7 +24,7 @@ func BuildTypeString() string {
 }
 
 // BuildVersion is the local build version
-const BuildVersion = "0.1.22"
+const BuildVersion = "0.1.23"
 
 func UserVersion() string {
 	if os.Getenv("TITAN_VERSION_IGNORE_COMMIT") == "1" {
